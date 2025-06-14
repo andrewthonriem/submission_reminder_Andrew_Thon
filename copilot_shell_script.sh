c60dc279b3dfa2b13c5569a3e6b9f292999a41b5
andrewthonriem <a.alier@alustudent.com>
2025-06-14 11:54:09 +0200

    Updated

--- copilot_shell_script.sh
+++ copilot_shell_script.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 
 DIR=$(find . -type d -name "submission_reminder_*" | head -n 1)
 
@@ -7,15 +7,14 @@ if [ -z "$DIR" ]; then
   exit 1
 fi
 
-echo "Please enter your new assignment name:"
+echo "Enter the new assignment name:"
 read new_assignment
 
 CONFIG_FILE="$DIR/config/config.env"
+sed -i "s/^ASSIGNMENT=.*/ASSIGNMENT=\"$new_assignment\"/" "$CONFIG_FILE"
 
-sed -i "s/^ASSIGNMENT=.*/ASSIGNMENT=$new_assignment/" "$CONFIG_FILE"
+echo "Assignment name updated to \"$new_assignment\"."
 
-echo "Assignment name updated to $new_assignment."
-
-bash "$DIR/startup.sh"
-
+cd "$DIR"
+bash "./startup.sh"
 
