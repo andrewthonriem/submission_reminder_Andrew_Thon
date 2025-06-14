192af65f5e9beb7309ea3ffade91d41b2b7693aa
andrewthonriem <a.alier@alustudent.com>
2025-06-14 10:31:11 +0200

    update

--- submission_reminder_Cyupe/modules/functions.sh
+++
@@ -1,8 +0,0 @@
-check_submissions() {
-    while IFS=',' read -r name assignment status
-    do
-        if [[ "$assignment" == "$ASSIGNMENT" && "$status" == "pending" ]]; then
-            echo "$name has not submitted $assignment"
-        fi
-    done < "$(dirname "$0")/../assets/submissions.txt"
-}
--- submission_reminder_Yom/startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-source "$DIR/config/config.env"
-source "$DIR/modules/functions.sh"
-bash "$DIR/app/reminder.sh"
