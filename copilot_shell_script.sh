8288d4003214e6d374c18a4cffb797a9e18c51cb
andrewthonriem <a.alier@alustudent.com>
2025-06-14 12:29:33 +0200

    updated

---
+++ copilot_shell_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIR=$(find . -type d -name "submission_reminder_*" | head -n 1)
+
+if [ -z "$DIR" ]; then
+  echo "Submission reminder directory not found."
+  exit 1
+fi
+
+echo "Enter the new assignment name:"
+read new_assignment
+
+CONFIG_FILE="$DIR/config/config.env"
+sed -i "s/^ASSIGNMENT=.*/ASSIGNMENT=\"$new_assignment\"/" "$CONFIG_FILE"
+
+echo "Assignment name updated to \"$new_assignment\"."
+
+cd "$DIR"
+bash "./startup.sh"
